15267fdbcb8252b56ecd27953c17c8f87a1fed93
barlowm <barlowm@gmail.com>
2019-01-27 17:43:20 -0500

    Add CSS modules and move files into separate folders

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # WebpackStarter
+##Master Branch
+
 A simple web app with an html template and 2 JS files to get the hang of working with Webpack, webpack-dev-server and the html-webpack-plugin
 
+- 
+  https://github.com/jantimon/html-webpack-plugin
+- https://github.com/webpack/webpack-dev-server
 
-https://github.com/jantimon/html-webpack-plugin
+The index.js file loads all the required components for the app and Webpack figures out what to do with them.
+
+## Add_CSS Branch
+
+Adding simple CSS module to the app 
+
+- https://webpack.js.org/guides/asset-management
+
+
+The index.js file even has to "require" any assets such as style sheets. Webpack figures out what to do with these as well.
+
+Only problem I see so far is that the screen flashes with a white background before the custom styles take effect since they're loaded as part of the JS file which is loaded after the entire page is rendered.
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
 
 module.exports = {
-  entry: {
-    filename: './src/index.js'    // JS file to be pushed to build folder.
-  },
-  output: {
-    filename: './main.js'   // The JS file get's renamed
-  },
+	entry: {
+		filename: "./src/assets/scripts/index.js"    // JS file to be pushed to build folder.
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-		    template: './src/index.html',   // Here's the HTML file used as a template
-        filename: 'index.html'            // and pushed here, relative to root of the application
-    }),
-  ]
+	output: {
+		filename: "main.js",   // The JS file get"s renamed
+		path: path.resolve(__dirname, "dist")
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader"
+				]
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",   // Here"s the HTML file used as a template
+			filename: "index.html"            // and pushed here, relative to root of the application
+		}),
+	]
 }
